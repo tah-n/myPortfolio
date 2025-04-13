8db2dea348f68d5542990ee084426e030fca262b
tah-n <taherenjfi8@gmail.com>
2025-04-13 23:13:50 +0330

    final commit

--- app/components/SkillContainer.tsx
+++ app/components/SkillContainer.tsx
@@ -4,20 +4,9 @@ import SkillsSection from './SkillsSection';
 
 const SkillContainer = () => {
   return (
-    <div className='text-white w-full h-[550px] font-jura mt-32 border-t border-t-white/10'>
-        <div className='w-full h-full'>
+    <div className='text-white w-full !h-[350px] font-jura mt-32'>
+        <div className='relative w-full h-full flex items-center justify-center'>
             <SkillsSection />
-
-            <div className='w-full h-[150px] name '>
-               <h4 className='font-semibold mb-2'>Tools and Workflow</h4>
-               <div className='text-sm text-white/60 leading-5'>
-                <p>Git & GitHub</p> 
-                <p>Zustand</p> 
-                <p>GSAP</p>
-                <p>Responsive Design</p> 
-                <p>Performance Optimization</p> 
-               </div>
-            </div>
         </div>        
     </div>
   )
--- app/components/SkillsSection.tsx
+++ app/components/SkillsSection.tsx
@@ -1,19 +1,18 @@
 'use client'
 import { Canvas, useFrame } from '@react-three/fiber';
 import React, { useRef } from 'react'
-import SkillMesh from './SkillMesh';
-import * as THREE from 'three';
-import { Html } from '@react-three/drei';
-import LightOnStraightPath from './LightOnStraightPath';
-import SkillsCircle from './SkillsCircle';
+import FallingSkills from './FallingSkills';
+import { OrbitControls } from '@react-three/drei';
 
 const SkillsSection = () => {
     
     
   return (
     <div>
-      <Canvas className='' style={{height: '550px'}}>
-         <SkillsCircle />
+      <Canvas className='' style={{height: '450px'}} camera={{position: [0,0,-4]}}>
+         <ambientLight position={[0,0,0]} intensity={4} color={'white'} />
+         <pointLight position={[0,0,0]} intensity={5} color={'#c41fa1'} />
+         <FallingSkills />
       </Canvas>
     </div>
   )
